77ec0306a137b3884b9a2fe74c602af39d51154f
ege cirakman <147109332+egesamuray@users.noreply.github.com>
2025-10-10 00:35:59 +0300

    Update LRHR_dataset.py

--- codes/data/LRHR_dataset.py
+++ codes/data/LRHR_dataset.py
@@ -1,3 +1,5 @@
+
+import os
 import os.path
 import random
 import numpy as np
@@ -8,121 +10,156 @@ import data.util as util
 
 
 class LRHRDataset(data.Dataset):
-    '''
-    Read LR and HR image pairs.
-    If only HR image is provided, generate LR image on-the-fly.
-    The pair is ensured by 'sorted' function, so please check the name convention.
-    '''
+    """
+    LR/HR paired dataset with optional on-the-fly LR.
+    Adds:
+      - class_id (0=text/number, 1=face; default 0)
+      - robust grayscale -> 3ch
+      - val/test LR size guard (regen LR if mismatch)
+      - color-conversion bug fix
+    """
 
     def __init__(self, opt):
         super(LRHRDataset, self).__init__()
         self.opt = opt
         self.paths_LR = None
         self.paths_HR = None
-        self.LR_env = None  # environment for lmdb
+        self.LR_env = None
         self.HR_env = None
 
-        # read image list from subset list txt
+        # read image list
         if opt['subset_file'] is not None and opt['phase'] == 'train':
             with open(opt['subset_file']) as f:
-                self.paths_HR = sorted([os.path.join(opt['dataroot_HR'], line.rstrip('\n')) \
-                        for line in f])
+                self.paths_HR = sorted([os.path.join(opt['dataroot_HR'], line.rstrip('\n'))
+                                        for line in f])
             if opt['dataroot_LR'] is not None:
-                raise NotImplementedError('Now subset only supports generating LR on-the-fly.')
-        else:  # read image list from lmdb or image files
+                raise NotImplementedError('subset only supports generating LR on-the-fly.')
+        else:
             self.HR_env, self.paths_HR = util.get_image_paths(opt['data_type'], opt['dataroot_HR'])
             self.LR_env, self.paths_LR = util.get_image_paths(opt['data_type'], opt['dataroot_LR'])
 
         assert self.paths_HR, 'Error: HR path is empty.'
         if self.paths_LR and self.paths_HR:
             assert len(self.paths_LR) == len(self.paths_HR), \
-                'HR and LR datasets have different number of images - {}, {}.'.format(\
-                len(self.paths_LR), len(self.paths_HR))
+                'HR and LR datasets have different number of images - {}, {}.'.format(
+                    len(self.paths_LR), len(self.paths_HR))
 
         self.random_scale_list = [1]
+        self.scale = self.opt['scale']
+        self.HR_size = self.opt.get('HR_size', None)
+
+    def __len__(self):
+        return len(self.paths_HR)
+
+    @staticmethod
+    def _ensure_3ch(img):
+        if img.ndim == 2:
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        if img.shape[2] == 1:
+            img = np.repeat(img, 3, axis=2)
+        return img
+
+    @staticmethod
+    def _infer_class_id(hr_path, lr_path):
+        p = (hr_path or lr_path or "").lower()
+        if ("face" in p) or ("ffhq" in p) or ("celeba" in p):
+            return 1
+        if ("text" in p) or ("number" in p) or ("word" in p) or ("textzoom" in p):
+            return 0
+        return 0
 
     def __getitem__(self, index):
-        HR_path, LR_path = None, None
-        scale = self.opt['scale']
-        HR_size = self.opt['HR_size']
-
-        # get HR image
         HR_path = self.paths_HR[index]
-        img_HR = util.read_img(self.HR_env, HR_path)
-        # modcrop in the validation / test phase
-        if self.opt['phase'] != 'train':
-            img_HR = util.modcrop(img_HR, scale)
-        # change color space if necessary
-        if self.opt['color']:
-            img_HR = util.channel_convert(img_HR.shape[2], self.opt['color'], [img_HR])[0]
+        LR_path = self.paths_LR[index] if self.paths_LR else None
 
-        # get LR image
-        if self.paths_LR:
-            LR_path = self.paths_LR[index]
+        # ----- HR -----
+        img_HR = util.read_img(self.HR_env, HR_path)
+        img_HR = self._ensure_3ch(img_HR)
+
+        # modcrop in val/test
+        if self.opt['phase'] != 'train':
+            img_HR = util.modcrop(img_HR, self.scale)
+
+        # color space (HR first)
+        if self.opt.get('color'):
+            inC = img_HR.shape[2]
+            img_HR = util.channel_convert(inC, self.opt['color'], [img_HR])[0]
+
+        # ----- LR -----
+        if LR_path:
             img_LR = util.read_img(self.LR_env, LR_path)
-        else:  # down-sampling on-the-fly
-            # randomly scale during training
+            img_LR = self._ensure_3ch(img_LR)
+
+            # ensure LR matches HR/scale at val/test
+            if self.opt['phase'] != 'train':
+                HrH, HrW = img_HR.shape[:2]
+                exp = (HrH // self.scale, HrW // self.scale)
+                if img_LR.shape[0] != exp[0] or img_LR.shape[1] != exp[1]:
+                    img_LR = util.imresize_np(img_HR, 1 / self.scale, True)
+                    if img_LR.ndim == 2:
+                        img_LR = np.expand_dims(img_LR, axis=2)
+                    img_LR = self._ensure_3ch(img_LR)
+        else:
+            # on-the-fly LR
             if self.opt['phase'] == 'train':
                 random_scale = random.choice(self.random_scale_list)
                 H_s, W_s, _ = img_HR.shape
 
-                def _mod(n, random_scale, scale, thres):
-                    rlt = int(n * random_scale)
-                    rlt = (rlt // scale) * scale
-                    return thres if rlt < thres else rlt
+                def _mod(n, r, sc, th):
+                    rlt = int(n * r); rlt = (rlt // sc) * sc
+                    return th if (self.HR_size and rlt < th) else rlt
 
-                H_s = _mod(H_s, random_scale, scale, HR_size)
-                W_s = _mod(W_s, random_scale, scale, HR_size)
-                img_HR = cv2.resize(np.copy(img_HR), (W_s, H_s), interpolation=cv2.INTER_LINEAR)
-                # force to 3 channels
-                if img_HR.ndim == 2:
-                    img_HR = cv2.cvtColor(img_HR, cv2.COLOR_GRAY2BGR)
+                if self.HR_size:
+                    H_s = _mod(H_s, random_scale, self.scale, self.HR_size)
+                    W_s = _mod(W_s, random_scale, self.scale, self.HR_size)
+                    img_HR = cv2.resize(np.copy(img_HR), (W_s, H_s), interpolation=cv2.INTER_LINEAR)
+                    img_HR = self._ensure_3ch(img_HR)
 
-            H, W, _ = img_HR.shape
-            # using matlab imresize
-            img_LR = util.imresize_np(img_HR, 1 / scale, True)
+            img_LR = util.imresize_np(img_HR, 1 / self.scale, True)
             if img_LR.ndim == 2:
                 img_LR = np.expand_dims(img_LR, axis=2)
+            img_LR = self._ensure_3ch(img_LR)
 
-        if self.opt['phase'] == 'train':
-            # if the image size is too small
+        # ----- training crops & aug -----
+        if self.opt['phase'] == 'train' and self.HR_size:
             H, W, _ = img_HR.shape
-            if H < HR_size or W < HR_size:
-                img_HR = cv2.resize(
-                    np.copy(img_HR), (HR_size, HR_size), interpolation=cv2.INTER_LINEAR)
-                # using matlab imresize
-                img_LR = util.imresize_np(img_HR, 1 / scale, True)
+            if H < self.HR_size or W < self.HR_size:
+                img_HR = cv2.resize(np.copy(img_HR), (self.HR_size, self.HR_size), interpolation=cv2.INTER_LINEAR)
+                img_HR = self._ensure_3ch(img_HR)
+                img_LR = util.imresize_np(img_HR, 1 / self.scale, True)
                 if img_LR.ndim == 2:
                     img_LR = np.expand_dims(img_LR, axis=2)
+                img_LR = self._ensure_3ch(img_LR)
 
-            H, W, C = img_LR.shape
-            LR_size = HR_size // scale
-
-            # randomly crop
+            H, W, _ = img_LR.shape
+            LR_size = self.HR_size // self.scale
             rnd_h = random.randint(0, max(0, H - LR_size))
             rnd_w = random.randint(0, max(0, W - LR_size))
             img_LR = img_LR[rnd_h:rnd_h + LR_size, rnd_w:rnd_w + LR_size, :]
-            rnd_h_HR, rnd_w_HR = int(rnd_h * scale), int(rnd_w * scale)
-            img_HR = img_HR[rnd_h_HR:rnd_h_HR + HR_size, rnd_w_HR:rnd_w_HR + HR_size, :]
+            rnd_h_HR, rnd_w_HR = int(rnd_h * self.scale), int(rnd_w * self.scale)
+            img_HR = img_HR[rnd_h_HR:rnd_h_HR + self.HR_size, rnd_w_HR:rnd_w_HR + self.HR_size, :]
 
-            # augmentation - flip, rotate
-            img_LR, img_HR = util.augment([img_LR, img_HR], self.opt['use_flip'], \
-                self.opt['use_rot'])
+            img_LR, img_HR = util.augment([img_LR, img_HR],
+                                          self.opt.get('use_flip', False),
+                                          self.opt.get('use_rot', False))
 
-        # change color space if necessary
-        if self.opt['color']:
-            img_LR = util.channel_convert(C, self.opt['color'], [img_LR])[0] # TODO during val no definetion
+        # optional LR color conversion
+        if self.opt.get('color'):
+            inC_lr = img_LR.shape[2]
+            img_LR = util.channel_convert(inC_lr, self.opt['color'], [img_LR])[0]
 
-        # BGR to RGB, HWC to CHW, numpy to tensor
+        # BGR->RGB, HWC->CHW, float32
         if img_HR.shape[2] == 3:
             img_HR = img_HR[:, :, [2, 1, 0]]
             img_LR = img_LR[:, :, [2, 1, 0]]
         img_HR = torch.from_numpy(np.ascontiguousarray(np.transpose(img_HR, (2, 0, 1)))).float()
         img_LR = torch.from_numpy(np.ascontiguousarray(np.transpose(img_LR, (2, 0, 1)))).float()
 
-        if LR_path is None:
+        if not LR_path:
             LR_path = HR_path
-        return {'LR': img_LR, 'HR': img_HR, 'LR_path': LR_path, 'HR_path': HR_path}
 
-    def __len__(self):
-        return len(self.paths_HR)
+        class_id = self._infer_class_id(HR_path, LR_path)
+
+        return {'LR': img_LR, 'HR': img_HR,
+                'LR_path': LR_path, 'HR_path': HR_path,
+                'class_id': class_id}
